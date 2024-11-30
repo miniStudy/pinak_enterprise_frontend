9852c3293dbf12af1d86f739697861ca86ce3938
anilkathayat07 <kathayatanil3@gmail.com>
2024-11-30 17:04:29 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Banks from './components/Banks';
+import MachineTypes from './components/MachineTypes'
 import BaseLayout from './components/BaseLayout';
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/" element={<BaseLayout />}>
           <Route index element={<Home />} />
           <Route path="banks" element={<Banks />} />
+          <Route path="machine-types" element={<MachineTypes />} />
         </Route>
       </Routes>
     </Router>
